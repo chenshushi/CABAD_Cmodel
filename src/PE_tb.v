390df1c6d39c424d5fa83ed3e12100baafee56a5
Chen_SS <chenshushi98@163.com>
2021-07-04 23:22:49 +0800

    a. add PE_tb (need sim)

---
+++ src/PE_tb.v
@@ -0,0 +1,34 @@
+module PE_tb();
+reg                 r_clk       ;
+reg                 r_rst_n     ;
+reg [15:0]          r_picDat    ;
+reg [15:0]          r_weightDat ;
+wire[36:0]          w_result    ;
+wire[36:0]          w_valid     ;
+
+initial begin
+    r_clk       = 1'b0;
+    r_rst_n     = 1'b1;
+    r_picDat    = 16'd0;
+    r_weightDat = 16'd0;
+    #10
+    r_rst_n = 1'b0;
+    #2
+    r_rst_n = 1'b1;
+    #2
+    r_picDat    = 16'd1;
+    r_weightDat = 16'd1;
+end
+always begin
+    #5 r_clk = ~r_clk;
+end
+PE #(5,16,16,37) PE_u
+(
+.clk           (r_clk      ),
+.rst_n         (r_rst_n    ),
+.picDat        (r_picDat   ),
+.weightDat     (r_weightDat),
+.result        (w_result   ),
+.valid         (w_valid    )
+);
+endmodule
--- src/pe.v
+++ src/pe.v
@@ -2,17 +2,16 @@ module PE#(
     parameter    knl_size     = 5 ,
     parameter    data_width   = 16,
     parameter    weight_width = 16,
-    parameter    result_width = data_width + weight_width + knl_size
+    parameter    result_width = 37 // data_width + weight_width + knl_size
 )
 (
-    input clk,
-    input rst_n,
-    input [(data_width  -1) :0] picDat,
-    input [(weight_width-1) :0] weightDat,
-    output[(result_width-1) :0] result,
-    output valid
+    input                                       clk,
+    input                                       rst_n,
+    input [(data_width  -1) :0]                 picDat,
+    input [(weight_width-1) :0]                 weightDat,
+    output reg [(result_width-1) :0]            result,
+    output reg                                  valid
 );
-reg [(result_width-1) :0] result;
 
 wire [(data_width + weight_width):0]      w_tmp_result;
 mul mul_u(
@@ -26,14 +25,14 @@ reg [4:0] cnt;
 always@(posedge clk or negedge rst_n) begin
     if (!rst_n)
         cnt <= 5'd0;
-    else if (cnt = 5'd24)
+    else if (cnt == 5'd24)
         cnt <= 5'd0;
     else
         cnt <= cnt + 5'd1;
 end
 
 always@(posedge clk or negedge rst_n) begin
-    if (!rst_n || cnt = 5'd24)
+    if (!rst_n || cnt == 5'd24)
         result <= 0;     //data width ?
     else
         result <= result + w_tmp_result;

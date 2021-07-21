d03f147abfccb8c4c4f76330865925c682fb8d36
chenshushi <chenshushi98@163.com>
2021-07-22 01:57:36 +0800

    a. demo finish (need  debug)

--- CNN_ON_FPGA_Cmodel/main.cpp
+++ CNN_ON_FPGA_Cmodel/main.cpp
@@ -14,9 +14,9 @@ int main(int argc, char **argv){
     bais_t   bias_L1  [4]                                = {0};  // 4
     weight_t weight_L2[4][8][5][5]                       = {0};  // 4 * 8
     bais_t   bias_L2  [8]                                = {0};  // 8
-    weight_t weight_L3[8][16]                            = {0};  // 8 * 16
+    weight_t weight_L3[8][16][5][5]                      = {0};  // 8 * 16
     bais_t   bias_L3  [16]                               = {0};  // 16
-    weight_t weight_F1[16][100]                          = {0};  // 16 * 100
+    weight_t weight_F1[16][100][28][28]                  = {0};  // 16 * 100
     bais_t   bias_F1  [100]                              = {0};  // 100
     weight_t weight_F2[100][4]                           = {0};  // 100 * 4
     bais_t   bias_F2  [4]                                = {0};  // 4
@@ -85,10 +85,10 @@ int main(int argc, char **argv){
         for(int posXCur = 0; posXCur < 246; posXCur++){
             weight_t *datCurBlock = &datPic[posYCur][posXCur];
 
-            result_data_L1[0][posYCur][posXCur] = filter(datCurBlock, weight_L1[0],250);
-            result_data_L1[1][posYCur][posXCur] = filter(datCurBlock, weight_L1[1],250);
-            result_data_L1[2][posYCur][posXCur] = filter(datCurBlock, weight_L1[2],250);
-            result_data_L1[3][posYCur][posXCur] = filter(datCurBlock, weight_L1[3],250);
+            result_data_L1[0][posYCur][posXCur] = filter(datCurBlock, weight_L1[0],250) + bias_L1[0];
+            result_data_L1[1][posYCur][posXCur] = filter(datCurBlock, weight_L1[1],250) + bias_L1[1];
+            result_data_L1[2][posYCur][posXCur] = filter(datCurBlock, weight_L1[2],250) + bias_L1[2];
+            result_data_L1[3][posYCur][posXCur] = filter(datCurBlock, weight_L1[3],250) + bias_L1[3];
         }
     }
     // L1_pool
@@ -115,10 +115,11 @@ int main(int argc, char **argv){
             weight_t *datCurBlock_2 = &pic_data_L2[2][posYCur][posXCur];
             weight_t *datCurBlock_3 = &pic_data_L2[3][posYCur][posXCur];
             for(int i = 0; i < 8; i++) {
-                result_data_L2[i][posYCur][posXCur] = (filter(datCurBlock_0, weight_L2[0][i],123)
-                                                    +  filter(datCurBlock_1, weight_L2[1][i],123)
-                                                    +  filter(datCurBlock_2, weight_L2[2][i],123)
-                                                    +  filter(datCurBlock_3, weight_L2[3][i],123) + 2) >> 2;
+                result_data_L2[i][posYCur][posXCur] = filter(datCurBlock_0, weight_L2[0][i],123)
+                                                    + filter(datCurBlock_1, weight_L2[1][i],123)
+                                                    + filter(datCurBlock_2, weight_L2[2][i],123)
+                                                    + filter(datCurBlock_3, weight_L2[3][i],123)
+                                                    + bias_L2[i];
             }
         }
     }
@@ -135,24 +136,104 @@ int main(int argc, char **argv){
 
     //L3
     //                      chennel  pixel    pixel
-    weight_t result_data_L3 [16             ][119    ][119     ] = {0};
-    for(int posYCur = 0; posYCur < 119; posYCur++){
-        for(int posXCur = 0; posXCur < 119; posXCur++){
-            weight_t *datCurBlock_0 = &pic_data_L2[0][posYCur][posXCur];
-            weight_t *datCurBlock_1 = &pic_data_L2[1][posYCur][posXCur];
-            weight_t *datCurBlock_2 = &pic_data_L2[2][posYCur][posXCur];
-            weight_t *datCurBlock_3 = &pic_data_L2[3][posYCur][posXCur];
-            for(int i = 0; i < 8; i++) {
-                result_data_L2[i][posYCur][posXCur] = (filter(datCurBlock_0, weight_L2[0][i],123)
-                                                    +  filter(datCurBlock_1, weight_L2[1][i],123)
-                                                    +  filter(datCurBlock_2, weight_L2[2][i],123)
-                                                    +  filter(datCurBlock_3, weight_L2[3][i],123) + 2) >> 2;
+    weight_t result_data_L3 [16             ][56    ][56     ] = {0};
+    for(int posYCur = 0; posYCur < 56; posYCur++){
+        for(int posXCur = 0; posXCur < 56; posXCur++){
+            weight_t *datCurBlock_0 = &pic_data_L3[0][posYCur][posXCur];
+            weight_t *datCurBlock_1 = &pic_data_L3[1][posYCur][posXCur];
+            weight_t *datCurBlock_2 = &pic_data_L3[2][posYCur][posXCur];
+            weight_t *datCurBlock_3 = &pic_data_L3[3][posYCur][posXCur];
+            weight_t *datCurBlock_4 = &pic_data_L3[4][posYCur][posXCur];
+            weight_t *datCurBlock_5 = &pic_data_L3[5][posYCur][posXCur];
+            weight_t *datCurBlock_6 = &pic_data_L3[6][posYCur][posXCur];
+            weight_t *datCurBlock_7 = &pic_data_L3[7][posYCur][posXCur];
+            for(int i = 0; i < 16; i++) {
+                result_data_L3[i][posYCur][posXCur] = filter(datCurBlock_0, weight_L3[0][i],60)
+                                                    + filter(datCurBlock_1, weight_L3[1][i],60)
+                                                    + filter(datCurBlock_2, weight_L3[2][i],60)
+                                                    + filter(datCurBlock_3, weight_L3[3][i],60)
+                                                    + filter(datCurBlock_4, weight_L3[4][i],60)
+                                                    + filter(datCurBlock_5, weight_L3[5][i],60)
+                                                    + filter(datCurBlock_6, weight_L3[6][i],60)
+                                                    + filter(datCurBlock_7, weight_L3[7][i],60)
+                                                    + bias_L3[i];
             }
         }
     }
+    //L3 pool
+    weight_t pic_data_F1[16][28][28] = {0};  // have padding problem !!!
+    for (int Y = 0; Y <56-1; Y++) {
+        for (int X = 0; X < 56-1; X++) {
+            for (int i = 0; i < 16; i++) {
+                pic_data_F1[i][Y/2][X/2] = (result_data_L3[i][Y + 0][X + 0] + result_data_L3[i][Y + 0][X + 1]
+                                         +  result_data_L3[i][Y + 1][X + 0] + result_data_L3[i][Y + 1][X + 1] + 2) >> 2;
+            }
+        }
+    }
+    //F1
+    //                       chennel
+    weight_t result_data_F3 [100    ] = {0};
+    for(int posYCur = 0; posYCur < 28; posYCur++){
+        for(int posXCur = 0; posXCur < 28; posXCur++){
+            weight_t *datCurBlock_0  = &pic_data_F1[0 ][posYCur][posXCur];
+            weight_t *datCurBlock_1  = &pic_data_F1[1 ][posYCur][posXCur];
+            weight_t *datCurBlock_2  = &pic_data_F1[2 ][posYCur][posXCur];
+            weight_t *datCurBlock_3  = &pic_data_F1[3 ][posYCur][posXCur];
+            weight_t *datCurBlock_4  = &pic_data_F1[4 ][posYCur][posXCur];
+            weight_t *datCurBlock_5  = &pic_data_F1[5 ][posYCur][posXCur];
+            weight_t *datCurBlock_6  = &pic_data_F1[6 ][posYCur][posXCur];
+            weight_t *datCurBlock_7  = &pic_data_F1[7 ][posYCur][posXCur];
+            weight_t *datCurBlock_8  = &pic_data_F1[8 ][posYCur][posXCur];
+            weight_t *datCurBlock_9  = &pic_data_F1[9 ][posYCur][posXCur];
+            weight_t *datCurBlock_10 = &pic_data_F1[10][posYCur][posXCur];
+            weight_t *datCurBlock_11 = &pic_data_F1[11][posYCur][posXCur];
+            weight_t *datCurBlock_12 = &pic_data_F1[12][posYCur][posXCur];
+            weight_t *datCurBlock_13 = &pic_data_F1[13][posYCur][posXCur];
+            weight_t *datCurBlock_14 = &pic_data_F1[14][posYCur][posXCur];
+            weight_t *datCurBlock_15 = &pic_data_F1[15][posYCur][posXCur];
+            for(int i = 0; i < 100; i++) {
+                result_data_F3[i]                   = result_data_F3[i]
+                                                    + pic_data_F1[0 ][posYCur][posXCur] * weight_L3[0 ][i][posYCur][posXCur]
+                                                    + pic_data_F1[1 ][posYCur][posXCur] * weight_L3[1 ][i][posYCur][posXCur]
+                                                    + pic_data_F1[2 ][posYCur][posXCur] * weight_L3[2 ][i][posYCur][posXCur]
+                                                    + pic_data_F1[3 ][posYCur][posXCur] * weight_L3[3 ][i][posYCur][posXCur]
+                                                    + pic_data_F1[4 ][posYCur][posXCur] * weight_L3[4 ][i][posYCur][posXCur]
+                                                    + pic_data_F1[5 ][posYCur][posXCur] * weight_L3[5 ][i][posYCur][posXCur]
+                                                    + pic_data_F1[6 ][posYCur][posXCur] * weight_L3[6 ][i][posYCur][posXCur]
+                                                    + pic_data_F1[7 ][posYCur][posXCur] * weight_L3[7 ][i][posYCur][posXCur]
+                                                    + pic_data_F1[8 ][posYCur][posXCur] * weight_L3[8 ][i][posYCur][posXCur]
+                                                    + pic_data_F1[9 ][posYCur][posXCur] * weight_L3[9 ][i][posYCur][posXCur]
+                                                    + pic_data_F1[10][posYCur][posXCur] * weight_L3[10][i][posYCur][posXCur]
+                                                    + pic_data_F1[11][posYCur][posXCur] * weight_L3[11][i][posYCur][posXCur]
+                                                    + pic_data_F1[12][posYCur][posXCur] * weight_L3[12][i][posYCur][posXCur]
+                                                    + pic_data_F1[13][posYCur][posXCur] * weight_L3[13][i][posYCur][posXCur]
+                                                    + pic_data_F1[14][posYCur][posXCur] * weight_L3[14][i][posYCur][posXCur]
+                                                    + pic_data_F1[15][posYCur][posXCur] * weight_L3[15][i][posYCur][posXCur]
+                                                    + bias_F1[i];
+            }
+        }
+    }
+    //F2
+    //                       chennel
+    weight_t result_data_F2 [4     ] = {0};
+    for(int i = 0; i < 100; i++){
+        result_data_F2[0] =result_data_F2[0] + result_data_F3[i] * weight_F2[i][0];
+        result_data_F2[1] =result_data_F2[1] + result_data_F3[i] * weight_F2[i][1];
+        result_data_F2[2] =result_data_F2[2] + result_data_F3[i] * weight_F2[i][2];
+        result_data_F2[3] =result_data_F2[3] + result_data_F3[i] * weight_F2[i][3];
+    }
+    result_data_F2[0] = result_data_F2[0] + bias_F2[0];
+    result_data_F2[1] = result_data_F2[1] + bias_F2[1];
+    result_data_F2[2] = result_data_F2[2] + bias_F2[2];
+    result_data_F2[3] = result_data_F2[3] + bias_F2[3];
 
-
-
+    //output
+    weight_t output_tmp1 = 0;
+    weight_t output_tmp2 = 0;
+    weight_t output      = 0;
+    output_tmp1 == result_data_F2[0] >= result_data_F2[1] ? result_data_F2[0] : result_data_F2[1];
+    output_tmp2 == result_data_F2[2] >= result_data_F2[3] ? result_data_F2[2] : result_data_F2[3];
+    output      == output_tmp1       >=  output_tmp2      ? output_tmp1       : output_tmp2      ;
 
     return 0;
 }
